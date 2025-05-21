c5e74133bcafd4a787d8349d700630918762e6d6
ElStefanoNacho <steven.morlet51@gmail.com>
2025-05-21 13:42:36 +0200

    Fix: Removed Next Oauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { Contexts } from '@/components/contexts'
 
 const geistSans = Geist({
    variable: '--font-geist-sans',
@@ -26,7 +25,7 @@ export default function RootLayout({
    return (
       <html lang="fr">
          <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-            <Contexts>{children}</Contexts>
+            {children}
          </body>
       </html>
    )

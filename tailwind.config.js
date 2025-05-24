e4dd2f08b29deef74ef15fe951e03def34ba3ec5
StevenMorlet <steven.morlet51@gmail.com>
2025-05-24 17:18:17 +0200

    Feat: Finalized background

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      fontFamily: {
-      },
+      fontFamily: {},
     },
   },
   plugins: [],

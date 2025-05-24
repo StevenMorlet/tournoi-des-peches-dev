aae434bf90c994f0a77044d950f631678eb8d5f9
StevenMorlet <steven.morlet51@gmail.com>
2025-05-24 18:41:45 +0200

    Feat: Added background to mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import Image from 'next/image';
+import background from '@/app/assets/background.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-6xl text-center font-[family-name:var(--font-geist-mono)]">
-        Le Tournoi des Péchés
-      </h1>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <div className="relative min-h-screen grid grid-rows-[20px_1fr_20px] justify-items-center items-center gap-16 px-8 pb-20 sm:px-20 font-[family-name:var(--font-geist-sans)]">
+      <Image
+        src={background}
+        alt="Background"
+        fill
+        quality={100}
+        className="object-cover z-0"
+        priority
+      />
+
+      <div className="absolute inset-0 bg-black/15 z-10" />
+
+      <main className="relative z-20 row-start-2 flex flex-col gap-8 items-center sm:items-start text-white text-center sm:text-left">
+        <h1 className="text-4xl sm:text-6xl font-[family-name:var(--font-geist-mono)] drop-shadow-xl">
+          Le Tournoi des Péchés
+        </h1>
+      </main>
+
+      <footer className="relative z-20 row-start-3 flex flex-wrap gap-6 items-center justify-center text-white text-sm">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
